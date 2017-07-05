edde1f8ca9c10a72aa0fc1dcb66ac7d0cf14d6e5
efeeley <elisabeth.k.feeley@gmail.com>
2017-07-05 15:24:25 -0400

    Add project files.

---
+++ ASSN709_EKF/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASSN709_EKF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare Variables
+            double hours, payrate, gross, overtime;
+            string hoursString, payrateString;
+
+            //User input
+            Console.WriteLine("Enter the amount of hours you worked: ");
+            hoursString = Console.ReadLine();
+            hours = Convert.ToDouble(hoursString);
+
+            Console.WriteLine("Enter your pay rate: ");
+            payrateString = Console.ReadLine();
+            payrate = Convert.ToDouble(payrateString);
+
+            //Process
+            if (hours <= 40)
+            {
+                gross = hours * payrate;
+                Console.WriteLine("Your gross pay is: ${0}", gross);
+                Console.ReadLine();
+            }
+            else if (hours > 40)
+            {
+                overtime = hours - 40;
+                gross = (overtime * (payrate * 1.5)) + (40 * payrate);
+                Console.WriteLine("Your gross pay is: ${0}", gross);
+                Console.ReadLine();
+            }//end if
+
+        }//end main
+    }//end class
+}//end program
